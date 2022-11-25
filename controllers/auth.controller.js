c6ac6bc377a252cdd718272b2277be5c8b2ddc6f
sakethgupta21 <sakethgupta123@gmail.com>
2022-11-25 21:08:57 +0530

    added signin code

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -8,7 +8,8 @@ const Role = db.role;
 const Op = db.Sequelize.Op;
 
 var jwt = require("jsonwebtoken")
-var bcrypt = require("bcryptjs")
+var bcrypt = require("bcryptjs");
+const { user } = require("../models");
 
 
 exports.signup = (req,res)=>{
@@ -39,5 +40,43 @@ exports.signup = (req,res)=>{
     })
     .catch(err=>{
         res.status(500).send("Unable to add the user " + err.message)
+    })
+}
+
+
+exports.signin=(req,res)=>{
+
+    User.findOne({
+        where:{
+            username:req.body.username
+        }
+    })
+    .then(user=>{
+
+        if(!user){
+            res.status(404).send({message:" User not Found"})
+        }
+        //check if the password is valid or not
+        var passwordIsValid = bcrypt.compareSync(req.body.password, user.password);
+
+        if(!passwordIsValid){
+            return res.status(401).send({
+                message: "Invalid Password"
+            })
+        }
+
+        //if both username and password are valid , then create a token
+
+        var token = jwt.sign({id: user.id}, config.secret,{expiresIn: 86400}) // 24Hrs
+
+        res.status(200).send({
+            username: user.username,
+            accestoken: token
+        })
+    })
+    .catch(err=>{
+        res.statu(500).send({
+            message: err.message
+        })
     })
 }
--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -5,6 +5,6 @@ const controller = require("../controllers/auth.controller");
 module.exports =function(app){
 
    app.post("/ecomm/api/v1/auth/signup",controller.signup);
-  // app.post("/ecomm/api/v1/auth/signin",controller.signin);
+   app.post("/ecomm/api/v1/auth/signin",controller.signin);
 
 }
